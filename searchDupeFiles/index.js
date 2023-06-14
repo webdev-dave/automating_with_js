5b23492f008f4c2e6ba2e8f31c510acdcc32369f
David Richard <elidovrich.coding@gmail.com>
2023-06-14 13:47:54 -0400

    begin searchDupeFiles project

--- searchDupeFiles/index.js
+++ searchDupeFiles/index.js
@@ -1,35 +1,55 @@
-const walk = require("walk");
-const path = require("path");
+// This script finds and deletes all duplicate files (duplicate as in duplicate file contents).
 
-const fileNameSizeCount = {}; // {size: path}
 
-const options = {
-  listeners: {
-    names: function (root, nodeNamesArray) {
-      nodeNamesArray.sort(function (a, b) {
-        if (a > b) return 1;
-        if (a < b) return -1;
-        return 0;
-      });
-    },
-    file: function (root, fileStats, next) {
-      const name = fileStats.name;
-      const size = fileStats.size;
-      const p = path.join(__dirname, root.slice(2), name);
 
-      if (size in fileNameSizeCount) {
-        console.log("Duplicate files found:");
-        console.log(p);
-        console.log(fileNameSizeCount[size]);
-        console.log("\n");
-      } else {
-        fileNameSizeCount[size] = p;
-      }
 
-      next();
-    },
-  },
-};
 
-walk.walkSync("./", options);
-console.log("all done");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const walk = require("walk");
+// const path = require("path");
+
+// const fileNameSizeCount = {}; // {size: path}
+
+// const options = {
+//   listeners: {
+//     names: function (root, nodeNamesArray) {
+//       nodeNamesArray.sort(function (a, b) {
+//         if (a > b) return 1;
+//         if (a < b) return -1;
+//         return 0;
+//       });
+//     },
+//     file: function (root, fileStats, next) {
+//       const name = fileStats.name;
+//       const size = fileStats.size;
+//       const p = path.join(__dirname, root.slice(2), name);
+
+//       if (size in fileNameSizeCount) {
+//         console.log("Duplicate files found:");
+//         console.log(p);
+//         console.log(fileNameSizeCount[size]);
+//         console.log("\n");
+//       } else {
+//         fileNameSizeCount[size] = p;
+//       }
+
+//       next();
+//     },
+//   },
+// };
+
+// walk.walkSync("./", options);
+// console.log("all done");

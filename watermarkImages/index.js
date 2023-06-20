b3da4798b436022f58ce7dbdf0406237679f456c
David Richard <elidovrich.coding@gmail.com>
2023-06-20 18:32:45 -0400

    begin watermarkImages project

--- watermarkImages/index.js
+++ watermarkImages/index.js
@@ -1,28 +1,79 @@
+//this script adds a watermark logo to all images in a given dir
+
 const fs = require("fs");
 const Jimp = require("jimp");
 
 const squareSize = 500;
 
-async function addLogoToImages() {
-  const logoImage = await Jimp.read("./images/propertyofkevin.png");
+async function addLogoToImages(){
+  const logoImage = await Jimp.read("./images/watermark_logo.png");
   logoImage.opacity(0.5);
 
-  const arrFileNames = fs.readdirSync("./images/non_watermarked");
-  for (const fileName of arrFileNames) {
+  const fileNamesArr = fs.readdirSync("./images/non_watermarked");
+  for (const fileName of fileNamesArr){
     const image = await Jimp.read(`./images/non_watermarked/${fileName}`);
-
+    
     image.resize(squareSize, squareSize);
-    console.log("Resizing " + fileName);
-
-    image.composite(logoImage, squareSize / 2, squareSize / 2);
-    console.log("Adding logo to " + fileName);
-
-    image.grayscale();
-    console.log("gray scaling " + fileName);
-
+    console.log("resizing " + fileName);
+    image.composite(logoImage, squareSize / 2, squareSize/2)
+    console.log("adding watermark to filename");
+    image.greyscale();
+    console.log("adding grey scale effect");
+    
+    //Save the newly watermarked img
     const imageFileName = "watermarked_" + fileName;
-    image.write("./images/watermarked/" + imageFileName);
+    image.write("./images/watermarked/"+ imageFileName);
   }
 }
 
 addLogoToImages();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const fs = require("fs");
+// const Jimp = require("jimp");
+
+// const squareSize = 500;
+
+// async function addLogoToImages() {
+//   const logoImage = await Jimp.read("./images/propertyofkevin.png");
+//   logoImage.opacity(0.5);
+
+//   const arrFileNames = fs.readdirSync("./images/non_watermarked");
+//   for (const fileName of arrFileNames) {
+//     const image = await Jimp.read(`./images/non_watermarked/${fileName}`);
+
+//     image.resize(squareSize, squareSize);
+//     console.log("Resizing " + fileName);
+
+//     image.composite(logoImage, squareSize / 2, squareSize / 2);
+//     console.log("Adding logo to " + fileName);
+
+//     image.grayscale();
+//     console.log("gray scaling " + fileName);
+
+//     const imageFileName = "watermarked_" + fileName;
+//     image.write("./images/watermarked/" + imageFileName);
+//   }
+// }
+
+// addLogoToImages();

6f1e1d62e2c05e6a6aa4cf866db9fbbe3f43b0f6
David Richard <elidovrich.coding@gmail.com>
2023-06-20 23:15:26 -0400

    update node packages to fix openApp import errors

--- workflowSetup/index.js
+++ workflowSetup/index.js
@@ -1,4 +1,6 @@
-import open from "open";
+// const open = require("open");
+// const {openApp} = require("open");
+import open, {openApp} from "open";
 
 const urls = [
   "https://github.com/webdev-dave",
@@ -7,9 +9,18 @@ const urls = [
   "https://www.codecademy.com/career-journey/full-stack-engineer/path/fscj-22-web-development-foundations/track/fscj-22-welcome-to-the-full-stack-engineer-path"
 ];
 
-//urls.forEach(url => open(url));
+// urls.forEach(url => open(url));
 
-open.openApp("Telegram");
+const telegramPath = "C:\\Users\\elido\\AppData\\Roaming\\Telegram Desktop\\Telegram.exe";
+const musicFile = "C:\\1 - Avrumy straus - Carlebach Shabbos Medley.mp3";
+
+open(musicFile);
+
+openApp(telegramPath);
+
+
+
+
 
 
 

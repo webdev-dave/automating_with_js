cb50e0328130ecdbc2d255a4e579ec452a7e9424
David Richard <elidovrich.coding@gmail.com>
2023-06-20 23:19:02 -0400

    complete workflowSetup project

--- workflowSetup/index.js
+++ workflowSetup/index.js
@@ -1,5 +1,3 @@
-// const open = require("open");
-// const {openApp} = require("open");
 import open, {openApp} from "open";
 
 const urls = [
@@ -9,14 +7,14 @@ const urls = [
   "https://www.codecademy.com/career-journey/full-stack-engineer/path/fscj-22-web-development-foundations/track/fscj-22-welcome-to-the-full-stack-engineer-path"
 ];
 
-// urls.forEach(url => open(url));
+urls.forEach(url => open(url));
 
-const telegramPath = "C:\\Users\\elido\\AppData\\Roaming\\Telegram Desktop\\Telegram.exe";
-const musicFile = "C:\\1 - Avrumy straus - Carlebach Shabbos Medley.mp3";
+//const telegramPath = "C:\\Users\\elido\\AppData\\Roaming\\Telegram Desktop\\Telegram.exe";
+//openApp(telegramPath);
 
-open(musicFile);
 
-openApp(telegramPath);
+
+
 
 
 

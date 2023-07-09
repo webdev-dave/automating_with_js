3d34a39809a58a463eacfccf0eb1df0ce88784f8
David Richard <elidovrich.coding@gmail.com>
2023-07-09 13:28:56 -0400

    complete csv_generator and export to index.js

--- emailCSV/csv_generator.js
+++ emailCSV/csv_generator.js
@@ -2,66 +2,36 @@ import * as fs from "fs";
 import { parse } from 'csv-parse/sync';
 import {stringify} from 'csv-stringify/sync';
 
-
-
-const CSV = fs.readFileSync("./csvFiles/USERS.csv");
-generateDomainCountCSV(CSV);
-
-function generateDomainCountCSV(usersCSV) {
+export function generateDomainCountCSV(usersCSV) {
   const usersArray = parse(usersCSV, {
     columns: true,
     skip_empty_lines: true,
   });
 
-  console.log(usersArray)
+  const domainCount = {}; // {gmail.com: 2};
+  for (const user of usersArray){
+    const domain = user.email.split("@")[1];
+    if(domain in domainCount){
+      domainCount[domain]++;    
+    } else {
+      domainCount[domain] = 1;
+    }
+  }
+
+  //init temp array of sub arrays that will become the csvFileWe export
+  //start with first subArray which contains the column names/titles/headers
+  const temp = [["domain", "count"]];
+  //add the remaining data into the subsequent rows 
+  for (const entry of Object.entries(domainCount)){
+    temp.push(entry)
+  }
+  //sort from most frequent domain names to least frequent
+  const columnNames = temp[0];
+  const sortedTemp = [columnNames,  ...temp.slice(1).sort((a,b) => b[1] - a[1])]
+
+  const data = stringify(sortedTemp);
+  fs.writeFileSync("./csvFiles/DOMAIN_COUNT.csv", data);
+
+
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const fs = require("fs");
-// const parse = require("csv-parse/lib/sync");
-// const stringify = require("csv-stringify/lib/sync");
-
-// function generateDomainCountCSV(usersCSV) {
-//   const usersArray = parse(usersCSV, {
-//     columns: true,
-//     skip_empty_lines: true,
-//   });
-
-//   const domainCount = {}; // {gmail.com: 5}
-
-//   // Fill out domainCount
-//   for (const user of usersArray) {
-//     const domain = user.email.split("@")[1];
-//     if (domain in domainCount) {
-//       domainCount[domain]++;
-//     } else {
-//       domainCount[domain] = 1;
-//     }
-//   }
-
-//   const temp = [["domain", "count"]];
-
-//   for (const entry of Object.entries(domainCount)) {
-//     temp.push(entry);
-//   }
-
-//   // Sort from greatest to least
-//   temp.sort((a, b) => b[1] - a[1]);
-
-//   const data = stringify(temp);
-//   fs.writeFileSync("./csvFiles/DOMAIN_COUNT.csv", data);
-// }
-
-// module.exports = { generateDomainCountCSV };
--- emailCSV/index.js
+++ emailCSV/index.js
@@ -1,17 +1,6 @@
-const fs = require("fs");
+import * as fs from "fs";
+import { generateDomainCountCSV } from "./csv_generator.js";
 
-const csvGenerator = require("./csv_generator");
-const sendMail = require("./send_email");
 
 const usersCSV = fs.readFileSync("./csvFiles/USERS.csv");
-
-// Generate output CSV
-csvGenerator.generateDomainCountCSV(usersCSV);
-// Grab output CSV that was just generated from above
-const domainCountCSV = fs.readFileSync("./csvFiles/DOMAIN_COUNT.csv");
-
-//Email output CSV
-sendMail("kaecsvemailproject@gmail.com", {
-  filename: "meow2",
-  content: domainCountCSV,
-});
+generateDomainCountCSV(usersCSV);

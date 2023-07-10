8a8a11e4c012d6b50c95d357ba2f28a2d984c33c
David Richard <elidovrich.coding@gmail.com>
2023-07-10 11:41:39 -0400

    intial web scrape country names successfull

--- webscraping/index.js
+++ webscraping/index.js
@@ -1,28 +1,56 @@
-const fs = require("fs");
-const axios = require("axios");
-const cheerio = require("cheerio");
+import * as fs from "fs";
+//note!! cheerio uses jQuery syntax
+import * as cheerio from 'cheerio';
+import axios from 'axios';
 
 const url = "https://en.wikipedia.org/wiki/List_of_countries_by_literacy_rate";
 
-async function fetchLiteracyRates() {
+
+fetchLiteracyRates()
+
+async function fetchLiteracyRates(){
   const res = await axios.get(url);
   const $ = cheerio.load(res.data);
   let fileString = "";
+  
+  const table = $('th:contains("Literacy rate")').parent().parent();
+  table.find("tbody tr").each((i, element)=>{
+    const $row = $(element);
 
-  const table = $('caption:contains("Literacy rate by country")').parent();
-  table
-    .find("tbody tr")
-    .slice(2)
-    .each((i, element) => {
-      const $row = $(element);
+    const countryName = $row.find("a").first().text();
+    console.log(countryName);
 
-      const countryName = $row.find("a").first().text();
-      const literacyRate = $row.find("td").slice(1, 2).text();
-
-      fileString += `${countryName}, ${literacyRate}\n`;
-    });
-
-  fs.writeFileSync("output.txt", fileString);
+  }) 
 }
 
-fetchLiteracyRates();
+
+
+
+
+
+
+
+
+
+// async function fetchLiteracyRates() {
+//   const res = await axios.get(url);
+//   const $ = cheerio.load(res.data);
+//   let fileString = "";
+
+//   const table = $('caption:contains("Literacy rate by country")').parent();
+//   table
+//     .find("tbody tr")
+//     .slice(2)
+//     .each((i, element) => {
+//       const $row = $(element);
+
+//       const countryName = $row.find("a").first().text();
+//       const literacyRate = $row.find("td").slice(1, 2).text();
+
+//       fileString += `${countryName}, ${literacyRate}\n`;
+//     });
+
+//   fs.writeFileSync("output.txt", fileString);
+// }
+
+// fetchLiteracyRates();

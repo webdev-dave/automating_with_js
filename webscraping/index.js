c8d70536fd636d876a167b82992f9896f2b706e4
David Richard <elidovrich.coding@gmail.com>
2023-07-10 12:52:44 -0400

    complete webscraping project

--- webscraping/index.js
+++ webscraping/index.js
@@ -1,5 +1,5 @@
 import * as fs from "fs";
-//note!! cheerio uses jQuery syntax
+// Note!! cheerio uses jQuery syntax
 import * as cheerio from 'cheerio';
 import axios from 'axios';
 
@@ -12,45 +12,23 @@ async function fetchLiteracyRates(){
   const res = await axios.get(url);
   const $ = cheerio.load(res.data);
   let fileString = "";
+  let countryListNumber = 0;
   
   const table = $('th:contains("Literacy rate")').parent().parent();
-  table.find("tbody tr").each((i, element)=>{
+  table.find("tbody tr").slice(2).each((i, element)=>{
     const $row = $(element);
+    countryListNumber++;
 
-    const countryName = $row.find("a").first().text();
-    console.log(countryName);
+    const countryName = $row.find('a').first().text().replace("*","").trim();
+    const literacyRate = $row.find("td").slice(1,2).text().trim();
+    //prevent scraping data from the any following tables with a similarly named header
+    if(countryListNumber > 195) return;
 
-  }) 
-}
+    if(countryName){
+      fileString += `${countryListNumber}) ${countryName}: ${literacyRate}`;
+    }
+  });
 
+  fs.writeFileSync("output.txt", fileString);
+};
 
-
-
-
-
-
-
-
-
-// async function fetchLiteracyRates() {
-//   const res = await axios.get(url);
-//   const $ = cheerio.load(res.data);
-//   let fileString = "";
-
-//   const table = $('caption:contains("Literacy rate by country")').parent();
-//   table
-//     .find("tbody tr")
-//     .slice(2)
-//     .each((i, element) => {
-//       const $row = $(element);
-
-//       const countryName = $row.find("a").first().text();
-//       const literacyRate = $row.find("td").slice(1, 2).text();
-
-//       fileString += `${countryName}, ${literacyRate}\n`;
-//     });
-
-//   fs.writeFileSync("output.txt", fileString);
-// }
-
-// fetchLiteracyRates();

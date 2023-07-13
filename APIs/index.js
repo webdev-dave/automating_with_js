d0d60eec978ef9e04d5f10d44ae445d307e4c11e
David Richard <elidovrich.coding@gmail.com>
2023-07-13 18:20:42 -0400

    complete APIs project

--- APIs/index.js
+++ APIs/index.js
@@ -1,43 +1,26 @@
-const fs = require("fs")
+const fs = require("fs");
 const axios = require("axios");
 const numeral = require("numeral");
 
-main()
-async function main(){
-  const countries = fs.readFileSync("input.txt").toString().split("\n").map(country => country.replace(/\r$/, ""))
-  console.log(countries)
-}
+main();
+async function main() {
+  const countries = fs
+    .readFileSync("input.txt")
+    .toString()
+    .split("\n")
+    .map((country) => country.replace(/\r$/, ""));
 
+  for (const country of countries) {
+    const url = `https://restcountries.com/v3.1/name/${country}`;
+    const response = await axios.get(url);
+    const data = response.data[0];
+    const name = data.name.common;
+    const subregion = data.subregion;
+    const population = numeral(data.population).format("0,0");
+    const outputData = `${name} is located in ${subregion} and has a population of ${population}`;
+    const outputFileName = name.toLowerCase().replace(/\s/, "_");
 
-
-
-
-
-
-
-
-
-
-// const fs = require("fs");
-// const axios = require("axios");
-// const numeral = require("numeral");
-
-// async function main() {
-//   const countries = fs.readFileSync("input.txt").toString().split("\n");
-
-//   for (const country of countries) {
-//     const url = `https://restcountries.eu/rest/v2/name/${country}?fullText=true`;
-//     const response = await axios.get(url);
-//     const data = response.data[0];
-
-//     const name = data.name;
-//     const subregion = data.subregion;
-//     const population = numeral(data.population).format("0,0");
-
-//     console.log(
-//       `${name} is located in ${subregion} and has a population of ${population}`
-//     );
-//   }
-// }
-
-// main();
+    fs.writeFileSync(`./output/${outputFileName}_output_data.txt`, outputData, (err) => { if (err) throw err; });
+  }
+  
+};

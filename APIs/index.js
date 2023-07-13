b142eb4afe09c5efec91d366d28cad63f84761df
David Richard <elidovrich.coding@gmail.com>
2023-07-13 11:38:54 -0400

    begin APIs project

--- APIs/index.js
+++ APIs/index.js
@@ -1,23 +1,43 @@
-const fs = require("fs");
+const fs = require("fs")
 const axios = require("axios");
 const numeral = require("numeral");
 
-async function main() {
-  const countries = fs.readFileSync("input.txt").toString().split("\n");
-
-  for (const country of countries) {
-    const url = `https://restcountries.eu/rest/v2/name/${country}?fullText=true`;
-    const response = await axios.get(url);
-    const data = response.data[0];
-
-    const name = data.name;
-    const subregion = data.subregion;
-    const population = numeral(data.population).format("0,0");
-
-    console.log(
-      `${name} is located in ${subregion} and has a population of ${population}`
-    );
-  }
+main()
+async function main(){
+  const countries = fs.readFileSync("input.txt").toString().split("\n").map(country => country.replace(/\r$/, ""))
+  console.log(countries)
 }
 
-main();
+
+
+
+
+
+
+
+
+
+
+// const fs = require("fs");
+// const axios = require("axios");
+// const numeral = require("numeral");
+
+// async function main() {
+//   const countries = fs.readFileSync("input.txt").toString().split("\n");
+
+//   for (const country of countries) {
+//     const url = `https://restcountries.eu/rest/v2/name/${country}?fullText=true`;
+//     const response = await axios.get(url);
+//     const data = response.data[0];
+
+//     const name = data.name;
+//     const subregion = data.subregion;
+//     const population = numeral(data.population).format("0,0");
+
+//     console.log(
+//       `${name} is located in ${subregion} and has a population of ${population}`
+//     );
+//   }
+// }
+
+// main();
